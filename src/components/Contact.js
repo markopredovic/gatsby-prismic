5c3969c94319afbdceb7db1ded39a48aea2be174
Marko Predovic <mpredovic@gmail.com>
2020-09-07 13:07:35 +0200

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from '@emotion/styled'
 import Img from 'gatsby-image'
-import { Box, Typography, Button } from '@material-ui/core'
+import {
+  Box,
+  Typography,
+  Button,
+  TextField,
+  useMediaQuery,
+} from '@material-ui/core'
+import Modal from '@material-ui/core/Modal'
+import Backdrop from '@material-ui/core/Backdrop'
+import Fade from '@material-ui/core/Fade'
+import { useTheme } from '@material-ui/core/styles'
 import BigTitle from './UI/BigTitle'
 
 const ContentBox = styled(Box)`
@@ -17,7 +27,42 @@ const ContentBox = styled(Box)`
   }
 `
 
+const ModalWrapper = styled(Modal)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const FormWrapperBox = styled(Box)`
+  ${({ theme }) => ({
+    width: '80%',
+    maxWidth: '550px',
+    padding: '2rem',
+    backgroundColor: '#fff',
+    color: '#000',
+    outlineColor: theme.palette.primary.light,
+    h6: { marginBottom: '1.5rem' },
+    '.MuiFormControl-root': { marginBottom: '1.5rem', width: '100%' },
+  })}
+`
+
 const Contact = ({ contact: { title, subtitle, description, image } }) => {
+  const [opened, setOpened] = useState(false)
+  const isMobile = useMediaQuery('(max-width: 599px)')
+  const theme = useTheme()
+
+  const handleOpen = () => {
+    setOpened(true)
+  }
+
+  const handleClose = () => {
+    setOpened(false)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
   return (
     <Box>
       <Box mb={4}>
@@ -34,9 +79,53 @@ const Contact = ({ contact: { title, subtitle, description, image } }) => {
         <Typography variant="body1">{description}</Typography>
       </ContentBox>
       <Box display="flex" flexDirection="column" alignItems="center">
-        <Button variant="contained" color="secondary">
+        <Button variant="contained" color="secondary" onClick={handleOpen}>
           Send an Email
         </Button>
+        <ModalWrapper
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={opened}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={opened}>
+            <FormWrapperBox theme={theme}>
+              <Typography variant="subtitle1">Contact us:</Typography>
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  id="fieldName"
+                  label="name"
+                  variant="outlined"
+                  size={isMobile ? 'small' : 'normal'}
+                />
+                <TextField
+                  id="fieldEmai"
+                  label="email"
+                  size={isMobile ? 'small' : 'normal'}
+                  variant="outlined"
+                />
+                <TextField
+                  id="standard-multiline-flexible"
+                  label="Message"
+                  multiline
+                  variant="outlined"
+                  size={isMobile ? 'small' : 'normal'}
+                  rows={4}
+                />
+                <Box display="flex" justifyContent="flex-end">
+                  <Button variant="contained" color="primary" type="submit">
+                    Send
+                  </Button>
+                </Box>
+              </form>
+            </FormWrapperBox>
+          </Fade>
+        </ModalWrapper>
       </Box>
     </Box>
   )
--- src/pages/playground/modal.js
+++ src/pages/playground/modal.js
@@ -1,12 +1,65 @@
 import React from 'react'
-import { Box } from '@material-ui/core'
+import { Box, Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import Modal from '@material-ui/core/Modal'
+import Backdrop from '@material-ui/core/Backdrop'
+import Fade from '@material-ui/core/Fade'
 
 import Layout from '../../components/layout'
 
+const useStyles = makeStyles(theme => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}))
+
 const ModalTestPage = () => {
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
   return (
     <Layout>
-      <Box>Modal</Box>
+      <Box px={2}>
+        <Button variant="contained" onClick={handleOpen}>
+          open modal
+        </Button>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <Box>
+              <h2 id="transition-modal-title">Transition modal</h2>
+              <p id="transition-modal-description">
+                react-transition-group animates me.
+              </p>
+            </Box>
+          </Fade>
+        </Modal>
+      </Box>
     </Layout>
   )
 }
